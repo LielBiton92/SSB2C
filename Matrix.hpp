6731adcadedc93668f4e9773159a1ee0b69c4398
LielBiton92 <92746221+LielBiton92@users.noreply.github.com>
2022-04-10 14:38:10 -0700

    Update Matrix.hpp

--- Matrix.hpp
+++ Matrix.hpp
@@ -6,13 +6,14 @@ namespace zich
     class Matrix
     {
     private:
-        int row;
-        int col;
+         int row;
+         int col;
 
     public:
-            std::vector<double> mat;
+             std::vector<double> mat;
 
         Matrix(int, int);
+        Matrix();
         Matrix(std::vector<double>, int row, int col);
         static void check_matrix_size(const Matrix &, const Matrix &);
         static void check_valid_mult(const Matrix &, const Matrix &);
@@ -25,11 +26,11 @@ namespace zich
 
         Matrix operator-();
         Matrix operator-(const Matrix &);
-        void operator-=(const Matrix &);
+        Matrix operator-=(const Matrix &);
 
         Matrix operator+();
         Matrix operator+(const Matrix &);
-        void operator+=(const Matrix &);
+        Matrix operator+=(const Matrix &);
 
         //-----------------------------
         // Friend Comparison Operators
@@ -46,26 +47,30 @@ namespace zich
         // Increment Decrement Operators
         //-----------------------------
 
-        void operator--();
-        void operator++();
-        void operator--(const int);
-        void operator++(const int);
+        Matrix operator--();
+        Matrix operator++();
+        Matrix operator--(const int);
+        Matrix operator++(const int);
 
         //-----------------------------
         // I/O Operators
         //-----------------------------
         friend std::ostream &operator<<(std::ostream &out, const Matrix &num);
-        friend std::istream &operator>>(std::istream &in, Matrix &num);
+        friend std::istream &operator>>(std::istream &in, Matrix &mat);
 
         //-----------------------------
         // Friend Operators & mult
         //-----------------------------
 
-        Matrix operator*(const double);
         Matrix operator*(const Matrix &);
-        void operator*=(const double);
-        void operator*=(const Matrix &);
+        Matrix operator*=(const double);
+        Matrix operator*=(const Matrix &);
+        friend bool operator==(const Matrix &mat1 , const Matrix &mat2);
+        std::vector<std::string> split(std::string const &str, char delim);
         friend Matrix operator*(double, const Matrix &);
+        friend Matrix operator*(const Matrix &, double);
+
+        std::string ostreamToString(std::istream &os);
     };
 
 }
